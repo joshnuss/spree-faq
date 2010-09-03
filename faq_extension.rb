99b70ed67485fbd67480802dba2009d1a63a911b
Christopher Maujean <cmaujean@gmail.com>
2010-09-02 21:14:19 -0700

    fixing for 0.11

--- faq_extension.rb
+++ faq_extension.rb
@@ -13,14 +13,6 @@ class FaqExtension < Spree::Extension
   # end
   
   def activate
-    Admin::ConfigurationsController.class_eval do
-      before_filter :add_faq_links, :only => :index
-      def add_faq_links
-        @extension_links << {:link => admin_question_categories_path, 
-                             :link_text => "Frequently Asked Questions", 
-                             :description => "Add,edit or remove FAQ categories and questions"}
-      end
-    end
   end
 
 end
