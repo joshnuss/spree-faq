428cb4feb267f0b96355f6b59e7ed6b5099cce2f
Alexander <alexander.negoda@gmail.com>
2011-10-18 22:20:27 +0400

    [DEPRECATION] `insert_bottom` hook method is deprecated

--- lib/spree_faq_hooks.rb
+++ lib/spree_faq_hooks.rb
@@ -1,6 +1,8 @@
 class SpreeFaqHooks < Spree::ThemeSupport::HookListener
-  insert_bottom :admin_tabs do
-    "<%=  tab(:question_categories, :label => :question_categories_admin) %>"
-  end
+  Deface::Override.new(:virtual_path => "layouts/admin",
+                     :name => "faq_admin_tab",
+                     :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
+                     :text => "<%=  tab(:question_categories, :label => :question_categories_admin) %>",
+                     :disabled => false)
 end
 
